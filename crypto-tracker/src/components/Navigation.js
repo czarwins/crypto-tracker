06c9be92b215d28236083f344135704b808aa464
czarwins <czarwins@gmail.com>
2019-05-29 07:46:07 -0400

    updated readme

--- crypto-tracker/src/components/Navigation.js
+++ crypto-tracker/src/components/Navigation.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import '../App.css'
 
 export default class Navigation extends Component {
     render() {
         return (
             <React.Fragment>
                 <div className="banner">
-                    <h3>CryptoTracker</h3>
+                    <h3 className="bannerText">CryptoTracker</h3>
                     <Link to={`/`} className="navLink">Home</Link>
                 </div>
             </React.Fragment>
